cab8a5e1ba22d6daf64df570648f3bd76bdf397a
刘伟 <lemonpaimc@126.com>
2022-05-11 14:05:15 +0800

    WIP:
    1.0.0

--- src/uploadFile.ts
+++ src/uploadFile.ts
@@ -1,17 +1,30 @@
 import * as core from '@actions/core'
 import Client from 'ssh2-sftp-client'
+import glob from 'glob'
+import path from 'path'
+import fs from 'fs'
 
 export async function uploadFile(
   client: Client,
   local: string,
   remote: string,
+  ignore: string[] = []
 ): Promise<boolean> {
-  return client.uploadDir(local, remote).then((res) => {
-    core.info(`UPLOAD FILE SUCCESS ----> ${res}`)
-    return true
-  })
-    .catch((e) => {
-      core.info(`UPLOAD FILE ERROR ----> ${e.message}`)
-      return false
+  const paths = glob.sync(path.join(local, '**/*'), { ignore: ignore || [] })
+  const files = paths.filter((f) => fs.statSync(f).isFile());
+  const dirs = paths.filter((f) => fs.statSync(f).isDirectory());
+  await Promise.all(dirs.map((d) => client.mkdir(d.replace(local, remote), true)))
+  return Promise.all(files.map((f) => {
+    return client.fastPut(f, f.replace(local, remote)).then((res) => {
+      core.info(`UPLOAD FILE SUCCESS ----> ${res}`)
+      return true
     })
+      .catch((e) => {
+        core.info(`UPLOAD FILE ERROR ----> ${e.message}`)
+        return false
+      })
+  })).then((status) => status.every((s) => !!s)).then((re) => {
+    client.end()
+    return re
+  })
 }
